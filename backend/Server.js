9bbb4a93ba97609c6179ad0723b60f1f0a4bc095
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-23 20:54:02 -0600

    fixing problem with server not connecting

---
+++ backend/Server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routesHandler = require('./routes/Handler.js');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/', routesHandler);
+
+//DB connection
+mongoose.connect(process.env.DB_URI, {useNewUrlparser:true, useUnifiedTopology:true})
+.then( ()=> {
+    console.log('DB connected');
+})
+.catch( (err) => {
+    console.log(err);
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log('Server is running on port 4000')
+});
