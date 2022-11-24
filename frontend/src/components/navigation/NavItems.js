e899aaca602dbe96406b272d955c2822ed141bc3
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-24 02:43:47 -0600

    routing front end

---
+++ frontend/src/components/navigation/NavItems.js
@@ -0,0 +1,19 @@
+export const NavItems= [
+    {
+        title: 'Login',
+        url: 'login',
+        cName: 'nav-links'
+    },
+    
+    {
+        title: 'Register',
+        url: 'register',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Journey',
+        url: 'journey',
+        cName: 'nav-links',
+    },
+]
