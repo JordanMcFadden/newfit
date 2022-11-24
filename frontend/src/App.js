55634900b2690f2d9d284954f3cac9ed954f4ac5
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-23 20:27:00 -0600

    Added both client and server side and mongoose

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          My First React APP
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
