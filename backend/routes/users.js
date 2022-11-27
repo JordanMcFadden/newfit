7165024c7e76c066ccfa549077dfa109dc9c0d97
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-27 13:58:59 -0600

    implemented and tested crud for users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,9 @@
 const { User, validate } = require('../models/user_model');
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
+const validObjectId = require('../middleware/validObjectId');
 
 router.post("/",async(req,res) => {
         const user = await User.findOne({email: req.body.email});
@@ -22,5 +25,34 @@ router.post("/",async(req,res) => {
 
         res.status(201).send({data: newUser, message:'User created successfully'})
 })
+
+// get all users
+router.get('/', admin, async(req, res) => {
+    const users = await User.find().select('-password -_v');
+    res.status(200).send({data: users});
+})
+
+// get user by id
+router.get('/:id', [validObjectId, auth], async(req, res) => {
+    const user = await User.findById(req.params.id).select('-password-_v');
+    res.status(200).send({data:user})
+})
+
+// update user by id
+router.put('/:id', [validObjectId, auth], async(req, res) => {
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {$set: req.body },
+        {new: true}
+    ).select("-password-_v");
+    res.status(200).send({data: user})
+})
+
+//delete user by id
+router.delete('/:id', [validObjectId, admin], async(req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send({message: 'Successfully deleted user'})
+});
+
 
 module.exports = router;
