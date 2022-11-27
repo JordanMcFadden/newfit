c4a98e43f545d56e2270a44c87463935564371d1
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-27 12:37:46 -0600

    commiting changes to switch to desktop

--- backend/middleware/aux.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-module.exports = (req, res, next) => {
-    const token = req.header('x-auth-token')
-    if(!token) {
-        return res.status(400).send({message: 'Access Denied, no token provided'})
-    }
-}
