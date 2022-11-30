1638bc0598e2b2446e719bf297567bd2dd2742ab
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-30 17:07:11 -0600

    small changes

---
+++ frontend/src/components/logout_component.js
@@ -0,0 +1,11 @@
+const Signout = () => {
+	return (
+		<div className={styles.main_container}>
+				<button className={styles.white_btn} >
+					Logout
+				</button>
+		</div>
+	);
+};
+
+export default Signout;
