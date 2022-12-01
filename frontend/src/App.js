faf9092439323ac75c35c3a0f73a618e19a8d358
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-30 20:08:58 -0600

    small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Routes, Route, Navigate} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Signin from './components/login_component';
 import Signup from './components/signup_component';
-import Home from './components/home_component';
+import Workout from './components/home_component';
+import Exercises from './components/exercise_component';
+import Records from './components/records_component';
 import Topbar from './components/topbar_component';
-import Personalize from './components/getstarted_component';
 
 
 function App() {
@@ -15,10 +16,11 @@ function App() {
            <Topbar /> 
           <div className="auth-inner">
             <Routes>
-              <Route path ="/" element={<Home />} />
-              <Route path="/get-started" element={<Personalize />} />
+              <Route exact path ="/" element={<Workout />} />
               <Route path="/sign-in" element={<Signin />}/>
               <Route path="/sign-up" element={<Signup />}/>
+              <Route path="/exercises" element={<Exercises />}/>
+              <Route path="/records" element ={<Records/>}/>
             </Routes>
           </div>
         </div>
---
+++ frontend/src/components/exercise_component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const exercise_component = () =>  {
+ 
+  return (
+    <div>exercise_component</div>
+  )
+}
+
+export default exercise_component
