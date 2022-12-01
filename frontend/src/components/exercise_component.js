d0cab6083bc1083c22d6431a2e49f59ffd29b92d
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-30 20:45:28 -0600

    small changes

--- frontend/src/components/exercise_component.js
+++ frontend/src/components/exercise_component.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Navbar from './navbar_component';
 
 const exercise_component = () =>  {
  
   return (
+    <>
+    <div><Navbar /></div>
     <div>exercise_component</div>
+    </>
   )
 }
 
